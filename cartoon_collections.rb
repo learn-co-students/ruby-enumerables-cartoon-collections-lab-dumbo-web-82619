9b36a6e33e82484dde76cf623f121b661213f53e
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-02 21:00:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,10 +13,13 @@ def long_planeteer_calls(arr) # code an argument here
   arr.any? {|word| word.length > 4}
 end
 
-def find_the_cheese(arr)# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  arr.include?(cheese_types)
-  
-  nil 
+
+
+def find_the_cheese(possibly_cheese_items)
+  cheeses = ["gouda", "cheddar", "camembert"]
+
+  possibly_cheese_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
+
